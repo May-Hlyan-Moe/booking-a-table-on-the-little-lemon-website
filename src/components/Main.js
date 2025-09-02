be6bcd6e80d379c38d58777b4242336a78df2b1f
May-Hlyan-Moe <129745420+May-Hlyan-Moe@users.noreply.github.com>
2025-09-02 16:21:00 +0630

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -50,7 +50,6 @@ const Main = () => {
     }
     return (
         <>
-            <Header /> 
             <main>
                 <Routes>
                     <Route path="" element={<Header />} />
