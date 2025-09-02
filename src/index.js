ebcaf1ed511368770b40d8c19ce96399927be848
May-Hlyan-Moe <129745420+May-Hlyan-Moe@users.noreply.github.com>
2025-09-02 16:14:10 +0630

    update

--- src/index.js
+++ src/index.js
@@ -8,11 +8,7 @@ import { BrowserRouter  } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-<<<<<<< Updated upstream
-    <BrowserRouter basename="/booking-a-table-on-the-little-lemon-website">
-=======
     <BrowserRouter basename='booking-a-table-on-the-little-lemon-website'>
->>>>>>> Stashed changes
       <App />
     </BrowserRouter>
   </React.StrictMode>
